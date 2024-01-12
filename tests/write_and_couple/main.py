3feb3675afb6cddb5209edb1b2fa9e8c11392516
Alexis Barge <alexis.barge@proton.me>
2024-01-12 19:15:18 +0100

    documentation added for tests

---
+++ tests/write_and_couple/main.py
@@ -0,0 +1,95 @@
+# eophis API
+import eophis
+from eophis import Freqs, Grids
+# other modules
+import argparse
+import os
+
+def earth_info():
+    # coupling config 
+    # STATIC: send/receive may be done manually ONCE and are ignored in time loops
+    # NON-STATIC : manual send/receive won't work outside of time loops
+    tunnel_config = list()
+    tunnel_config.append( { 'label' : 'TO_EARTH', \
+                            'grids' : { 'eORCA05' : Grids.eORCA05, \
+                                        'lmdz' : (180,151,0,0)  }, \
+                            'exchs' : [ {'freq' : Freqs.HOURLY, 'grd' : 'eORCA05', 'lvl' : 1, 'in' : ['sst'], 'out' : ['sst_var'] },  \
+                                        {'freq' : Freqs.DAILY,  'grd' : 'eORCA05', 'lvl' : 3, 'in' : ['svt'], 'out' : ['svt_var'] },  \
+                                        {'freq' : Freqs.STATIC, 'grd' : 'eORCA05', 'lvl' : 1, 'in' : ['msk'], 'out' : [] } ] }
+            # optional      'es_aliases' : { 'sst' : 'EAR_SST', 'svt' : 'EAR_TEMP', 'sst_var' : 'EAR_SSTV', 'svt_var' : 'EARTEMPV'},  \
+            # optional      'im_aliases' : { 'sst' : 'EOP_SST', 'svt' : 'EOP_TEMP', 'sst_var' : 'EOP_SSTV', 'svt_var' : 'EOPTEMPV'}   }
+                        )               
+
+    # earth namelist
+    earth_nml = eophis.FortranNamelist(os.path.join(os.getcwd(),'earth_namelist'))
+    return tunnel_config, earth_nml
+
+
+def preproduction():
+    eophis.info('========= EOPHIS DEMO : Pre-Production =========')
+    eophis.info('  Aim: write coupling namelist\n')
+
+    # earth info
+    tunnel_config, earth_nml = earth_info()
+    step, it_end, it_0 = earth_nml.get('rn_Dt','nn_itend','nn_it000')
+    total_time = (it_end - it_0 + 1) * step
+
+    # tunnel registration (lazy) compulsory to update namelist
+    eophis.register_tunnels( tunnel_config )
+    
+    # write updated namelist
+    eophis.write_coupling_namelist( simulation_time=total_time )
+
+
+def production():
+    eophis.info('========= EOPHIS DEMO : Production =========')
+    eophis.info('  Aim: execute coupled simulation\n')
+
+    #  Earth Coupling
+    # ++++++++++++++++
+    tunnel_config, earth_nml = earth_info()
+    step, it_end, it_0 = earth_nml.get('rn_Dt','nn_itend','nn_it000')
+    niter = it_end - it_0 + 1
+    total_time = niter * step
+
+    # tunnel registration (lazy)
+    earth, = eophis.register_tunnels( tunnel_config )
+
+    # link all tunnels (beware, dormant errors will likely appear here)
+    eophis.open_tunnels()
+
+    #  Models
+    # ++++++++
+    from models import add_100
+
+    # static send/receive
+    mask = earth.receive('msk')
+
+    #  Assemble
+    # ++++++++++
+    @eophis.all_in_all_out(earth_system=earth, step=step, niter=niter)
+    def loop_core(**inputs):
+        outputs = {}
+        outputs['sst_var'] = add_100(inputs['sst'])
+        outputs['svt_var'] = add_100(inputs['svt']) 
+        return outputs
+
+    #  Run
+    # +++++
+    eophis.starter(loop_core)
+    
+# ============================ #
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exec', dest='exec', type=str, default='prod', help='Execution type: preprod or prod')
+    args = parser.parse_args()
+
+    eophis.set_mode(args.exec)
+    
+    if args.exec == 'preprod':
+        preproduction()
+    elif args.exec == 'prod':
+        production()
+    else:
+        eophis.abort(f'Unknown execution mode {args.exec}, use "preprod" or "prod"')
---
+++ tests/write_and_couple/models.py
@@ -0,0 +1,27 @@
+"""
+Contains User Inference/Analytic Models or Functions.
+
+A model must fit the following requisites and structure :
+-------------------------------------------------------
+    1. must be a callable function that takes N numpy arrays as inputs
+    2. /!\ returns N None for the N awaited outputs if at least one of the input is None /!\
+    3. inputs may be freely formatted and transformed into what you want BUT...
+    4. ...outputs must be formatted as numpy array for sending back
+"""
+import numpy as np
+
+#             Utils            #
+# ++++++++++++++++++++++++++++ #
+def Is_None(*inputs):
+    """ Test presence of at least one None in inputs """
+    return any(item is None for item in inputs)
+
+# ============================ #
+#          Add Hundred         #
+# ============================ #
+def add_100(sst):
+    """ Trivially add 100 to sst (numpy.ndarray) """
+    if Is_None(sst):
+        return None
+    else:
+        return np.add(sst,100)
