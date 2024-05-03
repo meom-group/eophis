6087d2adf82846f0d48a275ff3b074604194b8c8
Alexis Barge <alexis.barge@proton.me>
2024-05-03 15:46:43 +0200

    update test_eophis.py

--- tests/test_eophis.py
+++ tests/test_eophis.py
@@ -1,7 +1,20 @@
 import eophis
 from eophis import Freqs, Grids
 import numpy as np
+import os
 import pytest
 
+def setup_function():
+    import eophis
+    from eophis import Freqs, Grids
 
+def teardown_function():
+    for file_name in ["eophis.out", "eophis.err"]:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+# Test presence of log files
+def check_log_files():
+    assert os.path.exists("eophis.out"), "log file 'eophis.out' does not exist"
+    assert os.path.exists("eophis.err"), "log file 'eophis.err' does not exist"
 
